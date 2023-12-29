6b24334513dbc3839ba3a83eb6a67c912b32121f
zigen <hrlclb@gmail.com>
2023-12-29 06:50:03 +0000

    conditional build

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#[cfg(any(
+    all(target_arch = "x86_64", target_os = "windows"),
+    all(target_arch = "x86_64", target_os = "linux")
+))]
 mod mhlib_wrapper;
 use mhlib_wrapper::{Edge, Mode, RefSource};
 use pyo3::exceptions::PyRuntimeError;
@@ -127,31 +131,41 @@ fn is_measurement_running(device_index: u8) -> PyResult<bool> {
     mhlib_wrapper::is_measurement_running(device_index).map_err(convert_into_py_err)
 }
 
+macro_rules! with_mhlib {
+    ($( $e:expr ),* ) => {
+        #[cfg(any(
+            all(target_arch = "x86_64", target_os = "windows"),
+            all(target_arch = "x86_64", target_os = "linux")
+        ))]
+        { $($e)*}
+    };
+}
+
 #[pymodule]
 #[pyo3(name = "_mhtk_rs")]
 fn _mhtk_rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(get_library_version, m)?)?;
-    m.add_function(wrap_pyfunction!(open_device, m)?)?;
-    m.add_function(wrap_pyfunction!(initialize, m)?)?;
-    m.add_function(wrap_pyfunction!(close_device, m)?)?;
-    m.add_function(wrap_pyfunction!(get_serial_number, m)?)?;
-    m.add_function(wrap_pyfunction!(get_number_of_input_channels, m)?)?;
-    m.add_function(wrap_pyfunction!(set_sync_divider, m)?)?;
-    m.add_function(wrap_pyfunction!(set_sync_edge_trigger, m)?)?;
-    m.add_function(wrap_pyfunction!(set_sync_channel_offset, m)?)?;
-    m.add_function(wrap_pyfunction!(set_input_edge_trigger, m)?)?;
-    m.add_function(wrap_pyfunction!(set_input_channel_enable, m)?)?;
-    m.add_function(wrap_pyfunction!(set_input_channel_offset, m)?)?;
-    m.add_function(wrap_pyfunction!(set_binning, m)?)?;
-    m.add_function(wrap_pyfunction!(set_offset, m)?)?;
-    m.add_function(wrap_pyfunction!(get_resolution, m)?)?;
-    m.add_function(wrap_pyfunction!(get_sync_rate, m)?)?;
-    m.add_function(wrap_pyfunction!(get_count_rate, m)?)?;
-    m.add_function(wrap_pyfunction!(get_warnings, m)?)?;
-    m.add_function(wrap_pyfunction!(start_measurement, m)?)?;
-    m.add_function(wrap_pyfunction!(stop_measurement, m)?)?;
-    m.add_function(wrap_pyfunction!(read_fifo, m)?)?;
-    m.add_function(wrap_pyfunction!(is_measurement_running, m)?)?;
+    with_mhlib!(m.add_function(wrap_pyfunction!(get_library_version, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(open_device, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(initialize, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(close_device, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(get_serial_number, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(get_number_of_input_channels, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(set_sync_divider, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(set_sync_edge_trigger, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(set_sync_channel_offset, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(set_input_edge_trigger, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(set_input_channel_enable, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(set_input_channel_offset, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(set_binning, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(set_offset, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(get_resolution, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(get_sync_rate, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(get_count_rate, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(get_warnings, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(start_measurement, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(stop_measurement, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(read_fifo, m)?)?);
+    with_mhlib!(m.add_function(wrap_pyfunction!(is_measurement_running, m)?)?);
 
     Ok(())
 }
